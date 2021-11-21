1a08fc52df8393af42e8f68d78c6b9a0ff509138
eitansh28 <92708274+eitansh28@users.noreply.github.com>
2021-11-21 12:11:28 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -20,15 +20,16 @@ int makeMat(){
         mat[i][j]=y;
       }
     }
-      for(int i = 0; i < 10; ++i)
-      {
-        for(int j = 0; j < 10; ++j)
-          printf("%d ", mat[i][j]);
-
-        printf("\n");
-
-
-    }return 0;
+    //   for(int i = 0; i < 10; ++i)
+    //   {
+    //     for(int j = 0; j < 10; ++j)
+    //       printf("%d ", mat[i][j]);
+    //
+    //     printf("\n");
+    //
+    //
+    // }
+    return 0;
 }
 
 
